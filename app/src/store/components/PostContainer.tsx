0dd18349b7d3a9842735efe5998f9ca5751acbb2
12Aleks <tanais2010@yahoo.com>
2022-04-05 21:31:43 +0200

    In test project was added jeson server, and new endpoint createPost

--- app/src/store/components/PostContainer.tsx
+++ app/src/store/components/PostContainer.tsx
@@ -4,13 +4,16 @@ import {postAPI} from "../services/PostService";
 import PostItem from "./PostItem";
 
 const PostContainer = () => {
-    const [limit, setLimit] = useState(10)
+    const [limit, setLimit] = useState(20)
     //hook ginerirujemyj automaticzeski na osnowanii opisywajemych endpoints
     //1 argument - paramentr ispolzujemyj w zaprose,
     const {data: posts, error, isLoading, refetch} = postAPI.useGetPostsQuery(limit,{
         //obnowlenie dannych czerez opredelennoje wrema
         pollingInterval: 10000
     });
+    //funkcija nazywajetsa po nazwaniu endpojeta sozdannogo nize
+    const [createPost, {error: createError, isLoading: isCreateLoading}] = postAPI.useCreatePostMutation();
+
 
     //testowoje izmenenije limita, keszyrowania dannych
     // useEffect(() => {
@@ -19,8 +22,14 @@ const PostContainer = () => {
     //     }, 3000)
     // }, []);
 
+    const handleCreate = async() => {
+       const title = prompt()
+        await createPost({title, body: title} as IPost)
+    };
+
     return (
         <div className='post__list'>
+            <button onClick={handleCreate}>Add new post</button>
             {isLoading && <h1>Loading ...</h1>}
             {error && <h1>Oh, error !!!</h1>}
             {/*Poluczenie obnowlonnych dannych*/}
--- app/src/store/services/PostService.ts
+++ app/src/store/services/PostService.ts
@@ -6,16 +6,30 @@ import {IPost} from "../models/IPost";
 
 export const postAPI = createApi({
   reducerPath: 'postApi',
-  baseQuery: fetchBaseQuery({baseUrl: 'https://jsonplaceholder.typicode.com/'}),
+  baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:5000/'}),
+  //javno ukazywajem @reduxjs/toolkit kuda dobawlat dannyje, mozem ukazat neskolko tegow
+  tagTypes: ['Post'],
   endpoints: (builder) => ({
       //wtoroj jenerik - tip argumenta kotoryj my dobawlaem (limit)
       getPosts: builder.query<IPost[], number>({
-          query: (limit: number = 5) => ({
+          query: (limit: number = 20) => ({
               url: '/posts',
               params: {
                   _limit: limit
               }
-          })
+          }),
+          //javno ukazywajem @reduxjs/toolkit kuda dobawlaet dannyje
+          providesTags: result => ['Post']
+      }),
+      //ima endpointa dajot nazwanie sozdawajemoj funkcii useCreatePostMutation()
+      createPost: builder.mutation<IPost, IPost>({
+          query: (post) => ({
+              url: '/posts',
+              method: 'POST',
+              body: post
+          }),
+          //ukazywajem javno czto etot endpoint obespecziwajet konkretnuju dostawku opredelonnych dannych ['Post]
+          invalidatesTags: ['Post']
       })
   })
 })
